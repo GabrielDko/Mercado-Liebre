30f6f01cd8623469d44ce74a9dd9029df16b50f7
GabrielDko <gabriielrobless@gmail.com>
2023-11-10 20:12:53 -0300

    arreglos form

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const bodyParser = require("body-parser");
 
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 const publicFolderPath = path.join(__dirname,"/public")
 app.use(express.static(publicFolderPath))
 
@@ -16,6 +18,15 @@ app.get("/register",(req,res)=>{
 
 app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+app.post("/register",(req,res)=>{
+    console.log(req.body)
+    res.redirect("/");
+})
+app.post("/login",(req,res)=>{
+    console.log(req.body)
+    res.redirect("/");
 })
 
 app.listen(3000, ()=> console.log("Servidor arrancado en el puerto 3000"))
