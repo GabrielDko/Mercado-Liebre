757c888c626cffa0bc1e7343a1f252867101fa30
GabrielDko <gabriielrobless@gmail.com>
2023-11-09 22:40:27 -0300

    formularios mercado liebre

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+
 const publicFolderPath = path.join(__dirname,"/public")
 app.use(express.static(publicFolderPath))
 
@@ -9,5 +10,12 @@ app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"))
 })
 
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
 
 app.listen(3000, ()=> console.log("Servidor arrancado en el puerto 3000"))
